79c8c5ef2be1254143552591de1f574ccf003980
just ivel554 <justivel554@justs-MacBook-Pro.local>
2025-06-28 20:28:22 +0300

    Complete Looping Lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// 1. writeCards function
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+// 2. countDown function
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
